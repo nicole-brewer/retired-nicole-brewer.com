907c8f435ffdc8c243e0a2bc829501efa49cdeb4
Nicole Brewer <brewer36@purdue.edu>
2020-07-30 22:37:33 -0400

    projects page separate from project

--- helpers.R
+++ helpers.R
@@ -1,5 +1,19 @@
 
+# CNAME
+# Custom slider widget
+# custom home header
+# list widget
+
 curl_markdown <- function(url) {
   output <- base::system(paste("curl", url), intern = TRUE)
+  output <- output[!startsWith(output, '# ')]
   knitr::combine_words(output, sep='\n')
+}
+
+clean_and_rebuild <- function() {
+  setwd('~/Repos/nicole-brewer-github/catch-me-coding/')
+  system('find content/ -name "*.html" -delete')
+  system('rm -rf public/*')
+  blogdown::build_site()
+  blogdown::hugo_build()
 }
